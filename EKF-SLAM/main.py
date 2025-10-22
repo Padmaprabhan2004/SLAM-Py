63eebb772302b050df915f299171419099ee66fc
A.Padmaprabhan <154980913+Padmaprabhan2004@users.noreply.github.com>
2025-10-22 12:33:34 +0530

    Add files via upload

---
+++ EKF-SLAM/main.py
@@ -0,0 +1,270 @@
+
+from lego_robot import *
+from math import sin, cos, pi, atan2, sqrt
+from numpy import *
+from slam_f_library import get_observations, write_cylinders,\
+     write_error_ellipses
+
+
+class ExtendedKalmanFilterSLAM:
+    def __init__(self, state, covariance,
+                 robot_width, scanner_displacement,
+                 control_motion_factor, control_turn_factor,
+                 measurement_distance_stddev, measurement_angle_stddev):
+        
+        self.state=state
+        self.covariance=covariance
+        self.robot_width= robot_width
+        self.scanner_displacement = scanner_displacement
+        self.control_motion_factor = control_motion_factor
+        self.control_turn_factor = control_turn_factor
+        self.measurement_distance_stddev = measurement_distance_stddev
+        self.measurement_angle_stddev = measurement_angle_stddev
+        self.number_of_landmarks = 0
+
+    @staticmethod
+    def g(state, control, w):
+        x, y, theta = state
+        l, r = control
+        if r != l:
+            alpha = (r - l) / w
+            rad = l/alpha
+            g1 = x + (rad + w/2.)*(sin(theta+alpha) - sin(theta))
+            g2 = y + (rad + w/2.)*(-cos(theta+alpha) + cos(theta))
+            g3 = (theta + alpha + pi) % (2*pi) - pi
+        else:
+            g1 = x + l * cos(theta)
+            g2 = y + l * sin(theta)
+            g3 = theta
+
+        return array([g1, g2, g3])
+
+    @staticmethod
+    def dg_dstate(state, control, w):
+        theta=state[2]
+        l, r=control
+        if r!=l:
+            alpha = (r-l)/w
+            theta_ = theta + alpha
+            rpw2 = l/alpha + w/2.0
+            m=array([[1.0, 0.0, rpw2*(cos(theta_) - cos(theta))],
+                       [0.0, 1.0, rpw2*(sin(theta_) - sin(theta))],
+                       [0.0, 0.0, 1.0]])
+        else:
+            m =array([[1.0, 0.0, -l*sin(theta)],
+                        [0.0, 1.0,  l*cos(theta)],
+                       [0.0, 0.0,  1.0]])
+        return m
+
+    @staticmethod
+    def dg_dcontrol(state, control, w):
+        theta = state[2]
+        l, r = tuple(control)
+        if r != l:
+            rml = r - l
+            rml2 = rml * rml
+            theta_ = theta + rml/w
+            dg1dl = w*r/rml2*(sin(theta_)-sin(theta))  - (r+l)/(2*rml)*cos(theta_)
+            dg2dl = w*r/rml2*(-cos(theta_)+cos(theta)) - (r+l)/(2*rml)*sin(theta_)
+            dg1dr = (-w*l)/rml2*(sin(theta_)-sin(theta)) + (r+l)/(2*rml)*cos(theta_)
+            dg2dr = (-w*l)/rml2*(-cos(theta_)+cos(theta)) + (r+l)/(2*rml)*sin(theta_)
+            
+        else:
+            dg1dl = 0.5*(cos(theta) + l/w*sin(theta))
+            dg2dl = 0.5*(sin(theta) - l/w*cos(theta))
+            dg1dr = 0.5*(-l/w*sin(theta) + cos(theta))
+            dg2dr = 0.5*(l/w*cos(theta) + sin(theta))
+
+        dg3dl = -1.0/w
+        dg3dr = 1.0/w
+        m = array([[dg1dl, dg1dr], [dg2dl, dg2dr], [dg3dl, dg3dr]])
+            
+        return m
+
+    def predict(self, control):
+
+        # covariance'=G*covariance*GT + R
+        # where R=V* (covariance in control space)*VTranspose.
+    
+        G3 = self.dg_dstate(self.state, control, self.robot_width)
+        left, right = control
+        left_var = (self.control_motion_factor * left)**2 +\
+                   (self.control_turn_factor * (left-right))**2
+        right_var = (self.control_motion_factor * right)**2 +\
+                    (self.control_turn_factor * (left-right))**2
+        control_covariance = diag([left_var, right_var])
+        V = self.dg_dcontrol(self.state, control, self.robot_width)
+        R3 = dot(V, dot(control_covariance, V.T))
+        n=self.number_of_landmarks
+        R3_glob=zeros((2*n+3,2*n+3))
+        R3_glob[:3,:3]=R3
+        G3_glob=eye(2*n+3)
+        G3_glob[:3,:3]=G3
+    
+        self.covariance = dot(G3_glob, dot(self.covariance, G3_glob.T)) + R3_glob  
+        
+        self.state[:3] = self.g(self.state[:3], control, self.robot_width)  
+
+    def add_landmark_to_state(self, initial_coords):
+        lx,ly=initial_coords[0],initial_coords[1]
+        self.state=hstack((self.state,[lx,ly]))
+        size=len(self.covariance)
+        cov=zeros((size+2,size+2))
+        cov[:size,:size]=self.covariance
+        cov[-2,-2]=10e10
+        cov[-1,-1]=10e10
+        self.covariance=cov
+        self.number_of_landmarks+=1
+        #print(self.state.shape)   
+        #print(self.number_of_landmarks) 
+
+        return self.number_of_landmarks-1
+
+
+    @staticmethod
+    def h(state, landmark, scanner_displacement):
+        dx = landmark[0] - (state[0] + scanner_displacement * cos(state[2]))
+        dy = landmark[1] - (state[1] + scanner_displacement * sin(state[2]))
+        r = sqrt(dx * dx + dy * dy)
+        alpha = (atan2(dy, dx) - state[2] + pi) % (2*pi) - pi
+        #measurement from lidar to glob frame
+        return array([r, alpha])
+
+    @staticmethod
+    def dh_dstate(state, landmark, scanner_displacement):
+        theta = state[2]
+        cost, sint = cos(theta), sin(theta)
+        dx = landmark[0] - (state[0] + scanner_displacement * cost)
+        dy = landmark[1] - (state[1] + scanner_displacement * sint)
+        q = dx * dx + dy * dy
+        sqrtq = sqrt(q)
+        drdx = -dx / sqrtq
+        drdy = -dy / sqrtq
+        drdtheta = (dx * sint - dy * cost) * scanner_displacement / sqrtq
+        dalphadx =  dy / q
+        dalphady = -dx / q
+        dalphadtheta = -1 - scanner_displacement / q * (dx * cost + dy * sint)
+
+        return array([[drdx, drdy, drdtheta],
+                      [dalphadx, dalphady, dalphadtheta]])
+
+    def correct(self, measurement, landmark_index):
+        
+    
+        landmark = self.state[3+2*landmark_index : 3+2*landmark_index+2]
+        H3 = self.dh_dstate(self.state, landmark, self.scanner_displacement)
+        n=self.number_of_landmarks
+        j=landmark_index
+        # set up the full H matrix for EKF-SLAM
+        H = zeros((2,len(self.state)))
+        #print(H[:,3:5].shape,landmark_index)
+        H[:,:3]=H3
+        H[:,3+2*j:3+2*j+2]=-H3[:,:2]
+        Q = diag([self.measurement_distance_stddev**2,
+                  self.measurement_angle_stddev**2])
+        K = dot(self.covariance,
+                dot(H.T, linalg.inv(dot(H, dot(self.covariance, H.T)) + Q)))
+        innovation = array(measurement) -\
+                     self.h(self.state, landmark, self.scanner_displacement)
+        innovation[1] = (innovation[1] + pi) % (2*pi) - pi
+        self.state = self.state + dot(K, innovation)
+        self.covariance = dot(eye(size(self.state)) - dot(K, H),
+                              self.covariance)
+
+    def get_landmarks(self):
+       
+        return ([(self.state[3+2*j], self.state[3+2*j+1])
+                 for j in range(self.number_of_landmarks)])
+
+    def get_landmark_error_ellipses(self):
+
+        ellipses = []
+        for i in range(self.number_of_landmarks):
+            j = 3 + 2 * i
+            ellipses.append(self.get_error_ellipse(
+                self.covariance[j:j+2, j:j+2]))
+        return ellipses
+
+    @staticmethod
+    def get_error_ellipse(covariance):
+
+        eigenvals, eigenvects = linalg.eig(covariance[0:2,0:2])
+        angle = atan2(eigenvects[1,0], eigenvects[0,0])
+        return (angle, sqrt(eigenvals[0]), sqrt(eigenvals[1]))        
+
+
+if __name__ == '__main__':
+    # Robot constants.
+    scanner_displacement = 30.0
+    ticks_to_mm = 0.349
+    robot_width = 155.0
+
+    # Cylinder extraction and matching constants.
+    minimum_valid_distance = 20.0
+    depth_jump = 100.0
+    cylinder_offset = 90.0
+    max_cylinder_distance = 500.0
+
+    # Filter constants.
+    control_motion_factor = 0.35  # Error in motor control.
+    control_turn_factor = 0.6  # Additional error due to slip when turning.
+    measurement_distance_stddev = 600.0  # Distance measurement error of cylinders.
+    measurement_angle_stddev = 45. / 180.0 * pi  # Angle measurement error.
+
+    # Arbitrary start position.
+    initial_state = array([500.0, 0.0, 45.0 / 180.0 * pi])
+
+    # Covariance at start position.
+    initial_covariance = zeros((3,3))
+
+    # Setup filter.
+    kf = ExtendedKalmanFilterSLAM(initial_state, initial_covariance,
+                                  robot_width, scanner_displacement,
+                                  control_motion_factor, control_turn_factor,
+                                  measurement_distance_stddev,
+                                  measurement_angle_stddev)
+
+    # Read data.
+    logfile = LegoLogfile()
+    logfile.read("robot4_motors.txt")#control data
+    logfile.read("robot4_scan.txt")#lidar data
+
+
+    with open("ekf_slam_correction.txt", "w") as f:
+        for i in range(len(logfile.motor_ticks)):
+            # Prediction.
+            control = array(logfile.motor_ticks[i]) * ticks_to_mm
+            kf.predict(control)
+
+            # Correction.
+            observations = get_observations(
+                logfile.scan_data[i],
+                depth_jump, minimum_valid_distance, cylinder_offset,
+                kf, max_cylinder_distance)
+            for obs in observations:
+                measurement, cylinder_world, cylinder_scanner, cylinder_index = obs
+                if cylinder_index == -1:
+                    cylinder_index = kf.add_landmark_to_state(cylinder_world)
+                kf.correct(measurement, cylinder_index)
+
+
+
+
+
+            # Output the center of the scanner, not the center of the robot.
+            f.write("F %f %f %f" % \
+                tuple(kf.state[0:3] + [scanner_displacement * cos(kf.state[2]),
+                                    scanner_displacement * sin(kf.state[2]),
+                                    0.0]))
+            f.write("\n")
+            # Write covariance matrix in angle stddev1 stddev2 stddev-heading form.
+            e = ExtendedKalmanFilterSLAM.get_error_ellipse(kf.covariance)
+            f.write("E %f %f %f %f" % (e + (sqrt(kf.covariance[2,2]),)))
+            f.write("\n")
+            # Write estimates of landmarks.
+            write_cylinders(f, "W C", kf.get_landmarks())
+            # Write error ellipses of landmarks.
+            write_error_ellipses(f, "W E", kf.get_landmark_error_ellipses())
+            # Write cylinders detected by the scanner.
+            write_cylinders(f, "D C", [(obs[2][0], obs[2][1])
+                                    for obs in observations])
